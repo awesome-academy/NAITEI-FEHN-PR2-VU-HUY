eb7b69b50e8d424ee80996d89ef564c015859c3a
yuherus <tuanhuy03ns@gmail.com>
2025-04-22 20:24:29 +0700

    home page

---
+++ components/ArticleCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-gray-900 rounded overflow-hidden">
+    <div class="h-48 overflow-hidden">
+      <NuxtImg 
+        :src="article.imageUrl" 
+        :alt="article.title" 
+        class="w-full h-full object-cover"
+        placeholder
+      />
+    </div>
+    <div class="p-4">
+      <h3 class="text-lg font-bold mb-2">{{ article.title }}</h3>
+      <p class="mb-3">{{ article.summary }}</p>
+      <p class="mb-3">Last updated {{ formatDate(article.updated_at)   }}</p>
+      <NuxtLink :to="`/news/${article.id}`" class="text-blue-400 font-[600] hover:text-blue-300">Read the full article</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { format, parseISO } from 'date-fns';
+defineProps({
+  article: {
+    type: Object,
+    required: true,
+    default: () => ({
+      title: '',
+      summary: '',
+      updated_at: '',
+      imageUrl: '',
+    })
+  }
+})
+
+const formatDate = (dateString) => {
+  const date = parseISO(dateString);
+  return format(date, 'dd MMM yyyy');
+};
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen flex flex-col bg-[radial-gradient(circle_at_center_top,#0a1c3a,#051122_900px,#051122)]">
     <Header />
 
-    <main class="flex-grow container max-w-6xl mx-auto px-4 py-8">
+    <main class="flex-grow py-8">
       <slot />
     </main>
 
